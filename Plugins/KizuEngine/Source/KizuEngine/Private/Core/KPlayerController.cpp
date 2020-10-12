2dde9ffa1deacfa8d1e2ff1f2a3014d21c89cb45
hirodemon <jedfakhfekh@gmail.com>
2020-10-12 03:19:40 +0100

    Removing useless files

--- Plugins/KizuEngine/Source/KizuEngine/Private/Core/KPlayerController.cpp
+++
@@ -1,8 +0,0 @@
-// KizuEngine Copyright (c) 2019 Jed Fakhfekh. This software is released under the MIT License.
-
-
-#include "Core/KPlayerController.h"
-#include "JoinSessionCallbackProxy.h"
-#include "CreateSessionCallbackProxy.h"
-#include "Interfaces/OnlineSessionInterface.h"
-
